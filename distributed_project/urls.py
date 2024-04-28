2ee1a4afc34e39968513b146891ab21918c10e32
Abdelrahman <abdelrahmanhamada65@gmail.com>
2024-04-28 11:04:01 +0300

    homepage apis

--- distributed_project/urls.py
+++ distributed_project/urls.py
@@ -22,6 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/home/", include("store.urls")),
     path("api/", include("authentication.urls")),
     path("", SpectacularSwaggerView.as_view()),
 ]
